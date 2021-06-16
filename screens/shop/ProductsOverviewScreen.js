80fbf082ac7d4ca88d385c0c8d3e15c12d11d60c
salim <zubairov.salim@yandex.ru>
2021-06-16 09:55:24 +0500

    added connection with Firebase-backend, implemented displaying and creating products with help of Redux-Thunk

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,21 +1,64 @@
-import React from 'react';
-import {Text, FlatList, Platform, Button, View} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Text, FlatList, Platform, Button, ActivityIndicator, View, StyleSheet} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
 import ProductItem from "../../components/shop/ProductItem";
 import * as cartActions from '../../store/actions/cart';
 import CustomHeaderButton from "../../components/UI/HeaderButton";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import Colors from "../../constants/Colors";
+import {fetchProducts} from "../../store/actions/products";
 
 const ProductsOverviewScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
     const products = useSelector(state => state.products.availableProducts);
     const dispatch = useDispatch();
+
+    const loadProducts = useCallback(async () => {
+        setError(null);
+        setIsLoading(true);
+        try {
+            await dispatch(fetchProducts());
+        } catch(error) {
+            setError(error.message);
+        }
+        setIsLoading(false);
+    }, [dispatch, setIsLoading, setError]);
+
+    useEffect(() => {
+        loadProducts();
+    }, [dispatch, loadProducts]);
 
     const selectItemHandler = (product) => {
         props.navigation.navigate('ProductDetail', {
             productId: product.id,
             productTitle: product.title
         });
+    }
+
+    if (error) {
+        return (
+            <View style={styles.centered}>
+                <Text>Ann error occurred!</Text>
+                <Button title='Try again' onPress={loadProducts} color={Colors.primary} />
+            </View>
+        )
+    }
+
+    if (isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size='large' color={Colors.primary} />
+            </View>
+        )
+    }
+
+    if (!isLoading && products.length === 0) {
+        return (
+            <View style={styles.centered}>
+                <Text>No products found. Maybe start adding some!</Text>
+            </View>
+        )
     }
 
     return (
@@ -67,5 +110,13 @@ ProductsOverviewScreen.navigationOptions = navData => {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default ProductsOverviewScreen;
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,5 +1,5 @@
 import PRODUCTS from "../../data/dummy-data";
-import {CREATE_PRODUCT, DELETE_PRODUCT, UPDATE_PRODUCT} from "../actions/products";
+import {CREATE_PRODUCT, DELETE_PRODUCT, SET_PRODUCTS, UPDATE_PRODUCT} from "../actions/products";
 import Product from "../../models/product";
 
 const initialState = {
@@ -9,10 +9,15 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case SET_PRODUCTS:
+            return {
+                availableProducts: action.products,
+                userProducts: action.products.filter(prod => prod.ownerId === 'u1')
+            }
         case CREATE_PRODUCT:
-            const {title, description, imageUrl, price} = action.productData;
+            const {id, title, description, imageUrl, price} = action.productData;
             const newProduct = new Product(
-                new Date().toString(),
+                id,
                 'u1',
                 title,
                 description,
