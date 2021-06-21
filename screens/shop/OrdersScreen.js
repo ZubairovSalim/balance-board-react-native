e72f5b1f75f8bc97b2c53e97fe93f5714369dfda
salim <zubairov.salim@yandex.ru>
2021-06-21 11:19:38 +0500

    implemented displaying and adding orders with help of Redux-Thunk, …added handling errors and loaders in CartScreen and OrdersScreen

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import {Text, FlatList, StyleSheet, Platform} from "react-native";
-import {useSelector} from "react-redux";
+import React, {useCallback, useEffect, useState} from 'react';
+import {Text, FlatList, StyleSheet, Platform, View, Button, ActivityIndicator} from "react-native";
+import {useSelector, useDispatch} from "react-redux";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import CustomHeaderButton from "../../components/UI/HeaderButton";
 import OrderItem from "../../components/shop/OrderItem";
+import {fetchOrders} from "../../store/actions/orders";
+import Colors from "../../constants/Colors";
 
 const OrdersScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+
     const orders = useSelector(state => state.orders.orders);
+    const dispatch = useDispatch();
+
+    const loadOrders = useCallback(async () => {
+        setError(null);
+        setIsLoading(true);
+        try {
+            await dispatch(fetchOrders());
+        } catch(error) {
+            setError(error.message);
+        }
+        setIsLoading(false);
+    }, [dispatch, setIsLoading, setError]);
+
+    // useEffect(() => {
+    //     const willFocusSubscription = props.navigation.addListener('willFocus', loadOrders);
+    //     return () => willFocusSubscription.remove();
+    // }, [loadOrders]);
+
+    useEffect(() => {
+        loadOrders();
+    }, [dispatch, loadOrders]);
+
+    if (error) {
+        return (
+            <View style={styles.centered}>
+                <Text>Ann error occurred!</Text>
+                <Button title='Try again' onPress={loadOrders} color={Colors.primary} />
+            </View>
+        )
+    }
+
+    if (isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size='large' color={Colors.primary} />
+            </View>
+        )
+    }
+
+    if (!isLoading && orders.length === 0) {
+        return (
+            <View style={styles.centered}>
+                <Text>No orders found. Maybe start adding some!</Text>
+            </View>
+        )
+    }
 
     return (
         <FlatList
@@ -37,5 +88,13 @@ OrdersScreen.navigationOptions = navData => {
         </HeaderButtons>
     )};
 }
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default OrdersScreen;
