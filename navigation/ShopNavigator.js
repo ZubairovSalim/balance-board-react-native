8d916b9dec0288e31e02068ccdf2faecba4162eb
salim <zubairov.salim@yandex.ru>
2021-06-30 22:10:11 +0500

    implemented auto-login with AsyncStorage and new StartupScreen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -12,6 +12,7 @@ import {Ionicons} from "@expo/vector-icons";
 import UserProductsScreen from "../screens/user/UserProductsScreen";
 import EditProductsScreen from "../screens/user/EditProductsScreen";
 import AuthScreen from "../screens/user/AuthScreen";
+import StartupScreen from "../screens/StartupScreen";
 
 
 const defaultNavOptions = {
@@ -89,6 +90,7 @@ const AuthNavigator = createStackNavigator({
 });
 
 const MainNavigator = createSwitchNavigator({
+    Startup: StartupScreen,
     Auth: AuthNavigator,
     Shop: ShopNavigator,
 });
---
+++ screens/StartupScreen.js
@@ -0,0 +1,47 @@
+import React, {useEffect} from 'react';
+import {View, ActivityIndicator, StyleSheet} from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Colors from "../constants/Colors";
+import {useDispatch} from "react-redux";
+import {authenticate} from "../store/actions/auth";
+
+const StartupScreen = props => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const tryLogin = async () => {
+            const userData = await AsyncStorage.getItem('userData');
+            if (!userData) {
+                props.navigation.navigate('Auth');
+                return;
+            }
+            const transformedData = JSON.parse(userData);
+            const {token, userId, expirationDate} = transformedData;
+            const expiryDate = new Date(expirationDate);
+
+            if (expiryDate <= new Date() || !token || !userData) {
+                props.navigation.navigate('Auth');
+                return;
+            }
+
+            props.navigation.navigate('Shop');
+            dispatch(authenticate(userId, token));
+        };
+        tryLogin();
+    }, [dispatch]);
+    return (
+        <View style={styles.screen}>
+            <ActivityIndicator size='large' color={Colors.primary} />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default StartupScreen;
